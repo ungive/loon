b82c05f2a998291d59f33187a28c0e1e0bbf02d8
Jonas van den Berg <jonas@vandenbe.com>
2024-07-19 00:03:06 +0200

    Update client example

--- clients/cpp/examples/client.cpp
+++ clients/cpp/examples/client.cpp
@@ -1,6 +1,7 @@
 #include "loon/client.h"
 
 #include <iostream>
+#include <sstream>
 #include <thread>
 
 using namespace std::chrono_literals;
@@ -13,20 +14,32 @@ int main()
     // options.min_cache_duration = (cache_duration * 4) / 5;
     // options.max_requests_per_second = 1.0f;
 
-    std::string address = "wss://localhost/ws";
+    loon::client_log_level(loon::LogLevel::Warning);
+    loon::websocket_log_level(loon::LogLevel::Error);
+
+    std::string address = "ws://localhost:8071/ws";
     loon::ClientOptions options;
-    options.websocket.basic_authorization =
-        "loon-client:qadjB4GeRyUSEjbj6ZFWwOiDtjLq";
-    options.websocket.ca_certificate_path =
-        "W:\\dev\\projects\\loon\\deployments\\cert.pem";
+    // options.websocket.basic_authorization =
+    //     "loon-client:qadjB4GeRyUSEjbj6ZFWwOiDtjLq";
+    // options.websocket.ca_certificate_path =
+    //     "W:\\dev\\projects\\loon\\deployments\\cert.pem";
     options.websocket.connect_timeout = 5000ms;
     options.websocket.ping_interval = 20000ms;
     options.websocket.reconnect_delay = 1000ms;
     options.websocket.max_reconnect_delay = 30000ms;
+    options.no_content_request_limit = std::make_pair(16, 1s);
     loon::Client client(address, options);
     client.start();
+    client.wait_until_connected();
 
-    std::string content = "<h1>It works!";
+    std::ostringstream oss;
+    oss << "<h1>It works!</h1><br>";
+    for (size_t i = 0; i < 1000; i++) {
+        oss << "<p>This is such an interesting thing to read!</p>";
+    }
+
+    // std::string content = "<h1>It works!";
+    std::string content = oss.str();
     std::string content_type = "text/html";
     std::vector<char> content_data(content.begin(), content.end());
     auto content_source =
@@ -40,9 +53,9 @@ int main()
         std::cout << "unregistered\n";
     });
 
-    std::cout << "URL: " << handle->url() << "\n";
+    std::cout << "URL: " << handle->url() << std::endl;
 
-    std::this_thread::sleep_for(300s);
+    std::this_thread::sleep_for(3s);
 
     std::cout << "Unregistering content" << std::endl;
     // client.unregister_content(handle);
@@ -51,6 +64,7 @@ int main()
     std::cout << "Stopping client" << std::endl;
     client.stop();
     std::this_thread::sleep_for(1s);
+    std::cout << "Exiting" << std::endl;
 
     // for (size_t i = 0; i < 32000; i++) {
     //     content += std::string("\n\n") + "okay";
