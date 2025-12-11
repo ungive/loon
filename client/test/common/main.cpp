a4a9e538067ebabbb340e49e74b8278197bfab8a
Jonas van den Berg <jonas@vandenbe.com>
2025-12-11 03:04:02 +0100

    Set RTC thread pool size to 1 for unit tests

--- client/test/common/main.cpp
+++ client/test/common/main.cpp
@@ -1,20 +1,14 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-#ifdef USE_QT
-#include <QCoreApplication>
-#include <QTimer>
+#include "rtc/global.hpp"
 
 int main(int argc, char* argv[])
 {
-    int qt_argc = 0;
-    QCoreApplication app(qt_argc, nullptr);
-    QTimer::singleShot(0, [&]() {
-        ::testing::InitGoogleTest(&argc, argv);
-        GTEST_FLAG_SET(death_test_style, "threadsafe");
-        auto testResult = RUN_ALL_TESTS();
-        app.exit(testResult);
-    });
-    return app.exec();
+    // Should be sufficient for unit tests.
+    rtc::SetThreadPoolSize(1);
+
+    ::testing::InitGoogleTest(&argc, argv);
+    GTEST_FLAG_SET(death_test_style, "threadsafe");
+    return RUN_ALL_TESTS();
 }
-#endif
